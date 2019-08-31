260fcf0fa51f52ae76fdd189895c692d664bafd8
maej <maej@maej-pc.wireless-1x.unc.edu>
2019-08-31 14:00:18 -0400

    completed assignment round one

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -4,11 +4,100 @@ import java.util.Scanner;
 
 public class A1Jedi {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+	Scanner scan = new Scanner(System.in);
+	int numEach = 0;
+	int numItems = 0;
+	String nameEach = "";
+	int numCust = 0;
+	
+	
+	
+	int storCount = scan.nextInt();
+	
+	
+	String[] ItemList = new String[storCount];
+	int[] ItemCount = new int[storCount];
+	int[] NumCustBought = new int[storCount];
+	
+	//*****************************************************************************
+	
+	//begin for loop populating ItemList array and accepting Item Price.
+	
+		for (int i = 0; i < storCount; i++) {
+		
+		
+			ItemList[i] = scan.next();
+			Double ItemPriceList = scan.nextDouble();	
+		
+		}//end for loop populating ItemList array and accepting Item Price.
+	 
+//**************************************************************************************
+		
+		numCust = scan.nextInt();
+		
+		for (int i = 0; i < numCust; i++){
+			
+			String firstName = scan.next();
+			String lastName = scan.next();
+			
+			numItems = scan.nextInt();
+			
+			for (int in = 0; in <numItems; in++) {
+				
+				numEach = scan.nextInt();
+				nameEach = scan.next();
+				
+				for (int ind = 0; ind < storCount; ind++) {
+					
+					if(nameEach.compareTo(ItemList[ind])==0) {
+						
+						ItemCount[ind] += numEach;
+						NumCustBought[ind]++;
+						
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+//************************************************************************************************		
+		//begin print loop
+					
+				for (int in = 0; in<storCount; in++) {
+					
+					if (NumCustBought[in]==0) {
+						
+						System.out.print("No customers bought ");
+						
+					}
+					
+					else {
+						
+					System.out.print(NumCustBought[in] + " customers bought ");
+					
+					}
+					
+					if(ItemCount[in]==0) {
+						
+						System.out.print("");
+					}
+					
+					else {
+						
+					System.out.print( ItemCount[in] + " ");
+					
+					}
+					System.out.println(ItemList[in] + " ");
+					
+					
+				}//end print loop
+				
+//***********************************************************************************************
 
-		// Your code follows here.
-		
-	}
-}
+ }//end main
+	
+}	
